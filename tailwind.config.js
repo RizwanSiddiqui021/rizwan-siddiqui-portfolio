f17151aee4c4807acac36883582a2b76f1c81a1f
RizwanSiddiqui021 <rizwansiddiqui021@gmail.com>
2025-02-04 23:13:58 +0600

    made changes in multiple sections

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        Outfit: ["Outfit", "serif"],
+        Ovo: ["Ovo", "serif"],
+      },
+      animation: {
+        spin_slow: "spin 6s linear infinite",
+      },
+      colors: {
+        lightHover: "#f2fdff",
+        darkHover: "#2a004a",
+      },
+    },
+  },
+};
