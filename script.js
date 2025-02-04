319d4832562306455e9fec925368386c7e59674a
RizwanSiddiqui021 <rizwansiddiqui021@gmail.com>
2025-02-04 19:19:08 +0600

    added the navbar blur effect

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-// mobile-menu-functions
 const sideMenu = document.querySelector("#sideMenu");
+const navBar = document.querySelector("nav");
+const navLinks = document.querySelector("nav ul");
 
+// mobile-menu-functions
 function openMenu() {
   sideMenu.style.transform = "translateX(-16rem)";
 }
@@ -8,4 +10,24 @@ function openMenu() {
 function closeMenu() {
   sideMenu.style.transform = "translateX(16rem)";
 }
-// mobile-menu-functions
+
+// nav-bar-functions
+window.addEventListener("scroll", () => {
+  if (scrollY > 50) {
+    navBar.classList.add(
+      "bg-white",
+      "bg-opacity-50",
+      "backdrop-blur-lg",
+      "shadow-sm"
+    );
+    navLinks.classList.remove("bg-white", "bg-opacity-50", "shadow-sm");
+  } else {
+    navBar.classList.remove(
+      "bg-white",
+      "bg-opacity-50",
+      "backdrop-blur-lg",
+      "shadow-sm"
+    );
+    navLinks.classList.add("bg-white", "bg-opacity-50", "shadow-sm");
+  }
+});
